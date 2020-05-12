667de88aba98e0a383ebb2f2fa59eb5afe99618d
Hailey <foxwavez@gmail.com>
2020-05-12 17:16:42 +0900

    AppDelegate에서 scene 관련된 거 삭제할니가 view에서 배경색 바꿔도 잘 작동됨

--- StarbucksClone/StarbucksClone/ViewController.swift
+++ StarbucksClone/StarbucksClone/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.backgroundColor = .white
     // Do any additional setup after loading the view.
   }
 
