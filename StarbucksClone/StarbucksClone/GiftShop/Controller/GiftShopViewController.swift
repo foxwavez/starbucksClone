e0c7dae5253c9ccad03b55a0e1b73638b6cc92f2
jisng <jxsxxng@gmail.com>
2020-05-21 10:19:07 +0900

    add: GSCollectionStyleView, GSBestNewCell

--- StarbucksClone/StarbucksClone/GiftShop/Controller/GiftShopViewController.swift
+++ StarbucksClone/StarbucksClone/GiftShop/Controller/GiftShopViewController.swift
@@ -21,10 +21,29 @@ class GiftShopViewController: UIViewController {
   }
   
   private func setupAttributes() {
-    
+    view.backgroundColor = .white
   }
   
   private func setupConstraints() {
-    
+    let testImage = UIImage(named: "test")!
+    let test: [[UIImage: String]] = [
+      [UIImage(named: "test") ?? testImage: "테스트 중간으로 모여라~"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"],
+      [UIImage(named: "test") ?? testImage: "테스트"]
+    ]
+    let bestView = GSCollectionStyleView(items: test, title: "BEST", subtitle: "가장 베스트트트ㅡ트트르르")
+    bestView.backgroundColor = .white
+    view.addSubview(bestView)
+    bestView.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(20)
+      $0.leading.trailing.equalToSuperview()
+      $0.height.equalTo(250)
+    }
   }
 }
---
+++ StarbucksClone/StarbucksClone/GiftShop/Views/Cells/GSThemeCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  GSThemeCollectionCell.swift
+//  StarbucksClone
+//
+//  Created by 박지승 on 2020/05/21.
+//  Copyright © 2020 momo. All rights reserved.
+//
+
+import UIKit
+
+class GSThemeCollectionCell: UICollectionViewCell {
+    
+}
