5a70ba9c1e994186b3530fb1dc3e586b95c76827
Hailey <foxwavez@gmail.com>
2020-05-27 23:53:13 +0900

    메뉴VC 생성

---
+++ StarbucksClone/StarbucksClone/MenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MenuViewController.swift
+//  StarbucksClone
+//
+//  Created by Hailey Lee on 2020/05/27.
+//  Copyright © 2020 momo. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+  private func setupUI() {
+    view.backgroundColor = .white
+  }
+  
+  
+}
