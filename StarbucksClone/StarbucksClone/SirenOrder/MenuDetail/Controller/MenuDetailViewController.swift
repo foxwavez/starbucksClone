44cc4ad8c9683789163c83e9bafc8e1f665b055f
jinging <zinging86@gmail.com>
2020-06-16 02:59:46 +0900

    feature: 사이렌오더상세페이지 - 기본정보 구현
    - 메뉴 이미지, 메뉴이름, 가격, 수량 구현

---
+++ StarbucksClone/StarbucksClone/SirenOrder/MenuDetail/Controller/MenuDetailViewController.swift
@@ -0,0 +1,96 @@
+//
+//  MenuDetailViewController.swift
+//  StarbucksClone
+//
+//  Created by Mac mini on 2020/06/16.
+//  Copyright © 2020 momo. All rights reserved.
+//
+
+import UIKit
+
+class MenuDetailViewController: UIViewController {
+
+  // MARK: Views
+  private let detailTableView = UITableView().then {
+    $0.separatorStyle = .none
+  }
+  
+  // MARK: Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+  
+  // MARK: Initialize
+  
+  private func setupUI() {
+    setupAttributes()
+    setupConstraints()
+  }
+  
+  private func setupAttributes() {
+    self.view.backgroundColor = .white
+    setupNavigationBar()
+    setupTableView()
+  }
+  private func setupNavigationBar() {
+    title = "메뉴 상세"
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "뒤로", style: .plain, target: self, action: nil)
+    navigationController?.navigationBar.tintColor = .white
+    let orderListButton = UIButton().then {
+      $0.setImage(UIImage(systemName: "bag"), for: .normal)
+      $0.imageView?.contentMode = .scaleToFill
+      $0.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
+    }
+    navigationItem.rightBarButtonItem = UIBarButtonItem(customView: orderListButton)
+  }
+  
+  private func setupTableView() {
+    self.detailTableView.delegate = self
+    self.detailTableView.dataSource = self
+    self.detailTableView.register(FirstBasicInformTableViewCell.self, forCellReuseIdentifier: FirstBasicInformTableViewCell.identifier)
+  }
+  
+  private func setupConstraints() {
+    let guide = view.safeAreaLayoutGuide
+
+    self.detailTableView.then { view.addSubview($0) }
+      .snp.makeConstraints {
+        $0.top.leading.trailing.bottom.equalTo(guide)
+    }
+  }
+}
+
+// MARK:- UITableViewDataSource
+
+extension MenuDetailViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    8
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.row {
+    case 0:
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: FirstBasicInformTableViewCell.identifier, for: indexPath) as? FirstBasicInformTableViewCell else { return UITableViewCell() }
+      cell.selectionStyle = .none
+      cell.backgroundColor = .white
+      return cell
+    default:
+      return UITableViewCell()
+    }
+  }
+}
+
+// MARK:- UITableViewDelegate
+
+extension MenuDetailViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    let viewWidth = UIScreen.main.bounds.width
+    switch indexPath.row {
+    case 0:
+      return viewWidth / 2.5
+    default:
+      return 0
+    }
+  }
+}
