64f67f81c8c5e2ccebda80eee0b8796cd9e98990
jisng <jxsxxng@gmail.com>
2020-06-17 17:24:58 +0900

    fix: navigation rightBarButtonItems layout

--- StarbucksClone/StarbucksClone/AppDelegate.swift
+++ StarbucksClone/StarbucksClone/AppDelegate.swift
@@ -20,24 +20,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
     window = UIWindow(frame: UIScreen.main.bounds)
-    let mainVC = UINavigationController(rootViewController: SirenOrderMainViewController()
+//    let mainVC = UINavigationController(rootViewController: SirenOrderMainViewController()
     //let mainVC = UINavigationController(rootViewController: MenuDetailViewController()
       
       
-      //    let mainVC = UINavigationController(rootViewController:
-      //      GiftShopViewController()
-      //    GSDetailViewController(seletedItem: GiftDetailItem(image: UIImage(named: "test")!, title: "테스트", subtitle: "이것은 테스트", price: 10000),
-      //                           otherItems: [
-      //                             [UIImage(named: "test")!: "무엇이냐1"],
-      //                             [UIImage(named: "test")!: "무엇이냐2"],
-      //                             [UIImage(named: "test")!: "무엇이냐3"],
-      //                             [UIImage(named: "test")!: "무엇이냐4"],
-      //                             [UIImage(named: "test")!: "무엇이냐5"],
-      //                             [UIImage(named: "test")!: "무엇이냐6"],
-      //                             [UIImage(named: "test")!: "무엇이냐7"],
-      //                             [UIImage(named: "test")!: "무엇이냐8"],
-      //                             [UIImage(named: "test")!: "무엇이냐9"]
-      //                           ])
+          let mainVC = UINavigationController(rootViewController:
+            GiftShopViewController()
+//          GSDetailViewController(seletedItem: GiftDetailItem(image: UIImage(named: "test")!, title: "테스트", subtitle: "이것은 테스트", price: 10000),
+//                                 otherItems: [
+//                                   [UIImage(named: "test")!: "무엇이냐1"],
+//                                   [UIImage(named: "test")!: "무엇이냐2"],
+//                                   [UIImage(named: "test")!: "무엇이냐3"],
+//                                   [UIImage(named: "test")!: "무엇이냐4"],
+//                                   [UIImage(named: "test")!: "무엇이냐5"],
+//                                   [UIImage(named: "test")!: "무엇이냐6"],
+//                                   [UIImage(named: "test")!: "무엇이냐7"],
+//                                   [UIImage(named: "test")!: "무엇이냐8"],
+//                                   [UIImage(named: "test")!: "무엇이냐9"]
+//                                 ])
     )
     
     mainVC.navigationBar.barStyle = .black
