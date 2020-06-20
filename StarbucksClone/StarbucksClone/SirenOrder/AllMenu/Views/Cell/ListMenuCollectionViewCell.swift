9ea6b7c7ced1beb60a136edacbafb063b3840d10
jinging <zinging86@gmail.com>
2020-06-21 06:12:55 +0900

    feature: 사이렌오더전체메뉴 - 리스트메뉴 구현
    - UI만 구현

--- StarbucksClone/StarbucksClone/SirenOrder/AllMenu/Views/Cell/ListMenuCollectionViewCell.swift
+++ StarbucksClone/StarbucksClone/SirenOrder/AllMenu/Views/Cell/ListMenuCollectionViewCell.swift
@@ -9,5 +9,56 @@
 import UIKit
 
 class ListMenuCollectionViewCell: UICollectionViewCell {
-    
+  
+  // MARK: Property
+  static let identifier = "listMenuCVC"
+  
+  // MARK: Views
+  private var listMenuTableView = UITableView()
+  
+  // MARK: Initialize
+  override init(frame: CGRect) {
+    super .init(frame: frame)
+    setupUI()
+  }
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupUI() {
+    setupAttributes()
+    setupConstraints()
+  }
+  
+  private func setupAttributes() {
+    setupTableView()
+  }
+  
+  private func setupTableView() {
+    self.listMenuTableView.delegate = self
+    self.listMenuTableView.dataSource = self
+    self.listMenuTableView.register(ListMenuTableViewCell.self, forCellReuseIdentifier: ListMenuTableViewCell.identifier)
+  }
+  
+  private func setupConstraints() {
+    self.listMenuTableView.then { self.contentView.addSubview($0) }
+      .snp.makeConstraints {
+        $0.top.equalToSuperview()
+        $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+}
+
+extension ListMenuCollectionViewCell: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 15
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = listMenuTableView.dequeueReusableCell(withIdentifier: ListMenuTableViewCell.identifier, for: indexPath) as? ListMenuTableViewCell else { return UITableViewCell() }
+    return cell
+  }
+}
+
+extension ListMenuCollectionViewCell: UITableViewDelegate {
 }
